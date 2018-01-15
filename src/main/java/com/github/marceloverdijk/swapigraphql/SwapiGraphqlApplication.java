9e77de664a3d7a78f9dfe21c63a5d951be52bde7
Marcel Overdijk <marcel@overdijk.me>
2018-01-15 20:55:53 +0100

    Renamed Application class

--- src/main/java/com/github/marceloverdijk/swapigraphql/SwapiGraphqlApplication.java
+++ src/main/java/com/github/marceloverdijk/swapigraphql/SwapiGraphqlApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwaphqlApplication {
+public class SwapiGraphqlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwaphqlApplication.class, args);
+        SpringApplication.run(SwapiGraphqlApplication.class, args);
     }
 }
