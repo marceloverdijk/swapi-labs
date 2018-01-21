e8fc5acfcc25a1c5fa103c14d77d7e50743243d8
Marcel Overdijk <marcel@overdijk.me>
2018-01-21 11:39:41 +0100

    Wip

--- swapi-labs-graphql/src/main/java/com/github/marceloverdijk/swapi/labs/graphql/query/Query.java
+++ swapi-labs-graphql/src/main/java/com/github/marceloverdijk/swapi/labs/graphql/query/Query.java
@@ -17,10 +17,18 @@
 package com.github.marceloverdijk.swapi.labs.graphql.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.github.marceloverdijk.swapi.labs.model.Film;
 import com.github.marceloverdijk.swapi.labs.model.Person;
 import com.github.marceloverdijk.swapi.labs.model.Planet;
+import com.github.marceloverdijk.swapi.labs.model.Species;
+import com.github.marceloverdijk.swapi.labs.model.Starship;
+import com.github.marceloverdijk.swapi.labs.model.Vehicle;
+import com.github.marceloverdijk.swapi.labs.repository.FilmRepository;
 import com.github.marceloverdijk.swapi.labs.repository.PersonRepository;
 import com.github.marceloverdijk.swapi.labs.repository.PlanetRepository;
+import com.github.marceloverdijk.swapi.labs.repository.SpeciesRepository;
+import com.github.marceloverdijk.swapi.labs.repository.StarshipRepository;
+import com.github.marceloverdijk.swapi.labs.repository.VehicleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,13 +50,23 @@ public class Query implements GraphQLQueryResolver {
 
     private static Logger LOGGER = LoggerFactory.getLogger(Query.class);
 
-    private PersonRepository personRepository;
-    private PlanetRepository planetRepository;
+    private final PersonRepository personRepository;
+    private final PlanetRepository planetRepository;
+    private final FilmRepository filmRepository;
+    private final SpeciesRepository speciesRepository;
+    private final VehicleRepository vehicleRepository;
+    private final StarshipRepository starshipRepository;
 
     @Autowired
-    public Query(final PersonRepository personRepository, final PlanetRepository planetRepository) {
+    public Query(final PersonRepository personRepository, final PlanetRepository planetRepository, final FilmRepository filmRepository,
+            final SpeciesRepository speciesRepository, final VehicleRepository vehicleRepository,
+            final StarshipRepository starshipRepository) {
         this.personRepository = Objects.requireNonNull(personRepository, "'personRepository' must not be null");
         this.planetRepository = Objects.requireNonNull(planetRepository, "'planetRepository' must not be null");
+        this.filmRepository = Objects.requireNonNull(filmRepository, "'filmRepository' must not be null");
+        this.speciesRepository = Objects.requireNonNull(speciesRepository, "'speciesRepository' must not be null");
+        this.vehicleRepository = Objects.requireNonNull(vehicleRepository, "'vehicleRepository' must not be null");
+        this.starshipRepository = Objects.requireNonNull(starshipRepository, "'starshipRepository' must not be null");
     }
 
     public String version() {
@@ -80,5 +98,57 @@ public class Query implements GraphQLQueryResolver {
         LOGGER.info("planet [{}] called", planetId);
         Planet planet = planetRepository.findById(planetId).orElse(null);
         return planet;
+    }
+
+    public List<Film> films(Integer page, Integer size) {
+        LOGGER.info("films called");
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.ASC, "title");
+        List<Film> films = filmRepository.findAll(pageable).getContent();
+        return films;
+    }
+
+    public Film film(Long filmId) {
+        LOGGER.info("film [{}] called", filmId);
+        Film film = filmRepository.findById(filmId).orElse(null);
+        return film;
+    }
+
+    public List<Species> species(Integer page, Integer size) {
+        LOGGER.info("species called");
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.ASC, "name");
+        List<Species> species = speciesRepository.findAll(pageable).getContent();
+        return species;
+    }
+
+    public Species getSpeciesById(Long speciesId) {
+        LOGGER.info("species [{}] called", speciesId);
+        Species species = speciesRepository.findById(speciesId).orElse(null);
+        return species;
+    }
+
+    public List<Vehicle> vehicles(Integer page, Integer size) {
+        LOGGER.info("vehicles called");
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.ASC, "name");
+        List<Vehicle> vehicles = vehicleRepository.findAll(pageable).getContent();
+        return vehicles;
+    }
+
+    public Vehicle vehicle(Long vehicleId) {
+        LOGGER.info("vehicle [{}] called", vehicleId);
+        Vehicle vehicle = vehicleRepository.findById(vehicleId).orElse(null);
+        return vehicle;
+    }
+
+    public List<Starship> starships(Integer page, Integer size) {
+        LOGGER.info("starships called");
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.ASC, "name");
+        List<Starship> starships = starshipRepository.findAll(pageable).getContent();
+        return starships;
+    }
+
+    public Starship starship(Long starshipId) {
+        LOGGER.info("starship [{}] called", starshipId);
+        Starship starship = starshipRepository.findById(starshipId).orElse(null);
+        return starship;
     }
 }
